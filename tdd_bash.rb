9a997decb21ea5e1fd11bb4dbfd9a80ea29607c7
Ricky Collins <richardcollins52317@gmail.com>
2018-06-26 10:41:03 -0400

    pass, test 1 match returns 1

--- tdd_bash.rb
+++ tdd_bash.rb
@@ -1,3 +1,11 @@
 def lottery_match(my_ticket, winning_ticket)
-    0
+    my_ticket = my_ticket.split('')
+    winning_ticket = winning_ticket.split('')
+    matching_numers = 0
+    winning_ticket.each_with_index do |num, index|
+        if num == my_ticket[index]
+            matching_numers += 1
+        end
+    end
+    matching_numers
 end
